9e1395eb1a08b601658f569e1335c67fef3dc837
Kevin Prakash <kevprakash@gmail.com>
2019-10-10 01:01:09 -0700

    Updated git repo to be up to date

--- LearnedActivationLayer.py
+++ LearnedActivationLayer.py
@@ -4,31 +4,19 @@ from keras import backend as K
 from keras.layers import Layer
 
 
-class LearnedActivationLayer(Layer):
+class TuningActivationLayer(Layer):
 
     def __init__(self, activationFunction, **kwargs):
         self._af = activationFunction
-        super(LearnedActivationLayer, self).__init__(**kwargs)
+        super(TuningActivationLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
         # Create a trainable weight variable for this layer.
-        self._xs = self.add_weight(name='horizontalScale',
-                                      shape=(1,),
-                                      initializer=keras.initializers.RandomNormal(mean=1.0, stddev=0.005, seed=None),
-                                      trainable=True)
-        self._xb = self.add_weight(name='horizontalBias',
-                                      shape=(1,),
-                                      initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.005, seed=None),
-                                      trainable=True)
-        self._ys = self.add_weight(name='verticalScale',
-                                      shape=(1,),
-                                      initializer=keras.initializers.RandomNormal(mean=1.0, stddev=0.005, seed=None),
-                                      trainable=True)
-        self._yb = self.add_weight(name='verticalBias',
-                                      shape=(1,),
-                                      initializer=keras.initializers.RandomNormal(mean=0.0, stddev=0.005, seed=None),
-                                      trainable=True)
-        super(LearnedActivationLayer, self).build(input_shape)  # Be sure to call this at the end
+        self._xs = self.add_weight(name='horizontalScale', shape=(1,), initializer=keras.initializers.Constant(value=1), trainable=True)
+        self._xb = self.add_weight(name='horizontalBias',  shape=(1,), initializer=keras.initializers.Constant(value=0), trainable=True)
+        self._ys = self.add_weight(name='verticalScale',   shape=(1,), initializer=keras.initializers.Constant(value=1), trainable=True)
+        self._yb = self.add_weight(name='verticalBias',    shape=(1,), initializer=keras.initializers.Constant(value=0), trainable=True)
+        super(TuningActivationLayer, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x, mask=None):
         return self._ys * self._af((self._xs * x) + self._xb) + self._yb
@@ -57,6 +45,57 @@ class LearnedFourierActivationLayer(Layer):
         s = 0
         for i in range(0, self._N):
             s = s + (self._p[i] * K.cos(2 * math.pi * i * x)) + (self._q[i] * K.sin(2 * math.pi * i * x))
+        return s
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class LearnedPolynomialActivationLayer(Layer):
+
+    def __init__(self, paramCount, **kwargs):
+        self._N = paramCount
+        super(LearnedPolynomialActivationLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+
+        self._p = self.add_weight(name='p', shape=(self._N,), initializer=keras.initializers.RandomNormal(mean=1, stddev=0.05), trainable=True)
+
+        self._q = self.add_weight(name='q', shape=(self._N,), initializer=keras.initializers.RandomNormal(mean=1, stddev=0.05), trainable=True)
+
+        super(LearnedPolynomialActivationLayer, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x, mask=None):
+
+        numerator = 0
+        denominator = 0
+        for i in range(0, self._N):
+            numerator += self._p[i] * K.pow(x, i)
+            denominator += self._q[i] * K.pow(x, i)
+        return numerator/denominator
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+class LearnedExponentialActivationLayer(Layer):
+
+    def __init__(self, paramCount, **kwargs):
+        self._N = paramCount
+        super(LearnedExponentialActivationLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+
+        self._p = self.add_weight(name='p', shape=(self._N,), initializer=keras.initializers.RandomNormal(mean=0, stddev=0.005), trainable=True)
+        self._q = self.add_weight(name='q', shape=(self._N,), initializer=keras.initializers.RandomNormal(mean=0, stddev=0.005), trainable=True)
+        super(LearnedExponentialActivationLayer, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x, mask=None):
+        s = 0
+        for i in range(0, self._N):
+            s += self._p[i]*K.exp(i * self._q[i] * x)
+
         return s
 
     def compute_output_shape(self, input_shape):
--- Tester.py
+++ Tester.py
@@ -2,108 +2,113 @@ import LearnedActivationLayer as LAL
 import keras
 import tensorflow as tf
 
-def LALTester(c=1, d=2, act=keras.activations.relu):
-    dataset = keras.datasets.cifar10
-    input_shape = (32, 32, 3)
-    reshape_shape = (32, 32, 3)
-    outputLength = 10
-    h = 32
-    drop = 0.0
+def controlModel(inputShape, c, d, h, outputLength, act=keras.activations.relu):
+    input = keras.layers.Input(shape=inputShape)
+    x = input
+    for _ in range(0, c[0]):
+        for _ in range(0, c[1]):
+            x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(1, 1), padding='same', activation=act)(x)
+        x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same', activation=act)(x)
+    x = keras.layers.Flatten()(x)
+    for _ in range(0, d):
+        x = keras.layers.Dense(units=h, activation=act)(x)
+    output = keras.layers.Dense(units=outputLength, activation=keras.activations.softmax)(x)
 
-    (train_images, train_labels), (test_images, test_labels) = dataset.load_data()
+    model = keras.Model(inputs=[input], outputs=[output])
+    model.compile(optimizer=keras.optimizers.RMSprop(lr=0.0002), loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
 
-    train_images = train_images / 255.0
+    return model
 
-    test_images = test_images / 255.0
 
-    input = keras.layers.Input(shape=input_shape)
-    reshape = keras.layers.Reshape(target_shape=reshape_shape)(input)
-    x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same')(reshape)
-    x = LAL.LearnedActivationLayer(activationFunction=act)(x)
-    x = keras.layers.Dropout(rate=drop)(x)
-    for _ in range(0, c):
-        x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same')(x)
-        x = LAL.LearnedActivationLayer(activationFunction=act)(x)
-        x = keras.layers.Dropout(rate=drop)(x)
+def tuningModel(inputShape, c, d, h, outputLength, act=keras.activations.relu):
+    input = keras.layers.Input(shape=inputShape)
+    x = input
+    for _ in range(0, c[0]):
+        for _ in range(0, c[1]):
+            x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(1, 1), padding='same', activation=None)(x)
+            x = LAL.TuningActivationLayer(activationFunction=act)(x)
+        x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same', activation=None)(x)
+        x = LAL.TuningActivationLayer(activationFunction=act)(x)
     x = keras.layers.Flatten()(x)
     for _ in range(0, d):
         x = keras.layers.Dense(units=h, activation=None)(x)
-        x = LAL.LearnedActivationLayer(activationFunction=act)(x)
+        x = LAL.TuningActivationLayer(activationFunction=act)(x)
     x = keras.layers.Dense(units=outputLength, activation=None)(x)
-    x = LAL.LearnedActivationLayer(activationFunction=keras.activations.softmax)(x)
+    x = LAL.TuningActivationLayer(activationFunction=keras.activations.softmax)(x)
 
     model = keras.Model(inputs=[input], outputs=[x])
-    model.compile(optimizer=keras.optimizers.RMSprop(), loss=keras.losses.sparse_categorical_crossentropy,
+    model.compile(optimizer=keras.optimizers.RMSprop(lr=0.0002), loss=keras.losses.sparse_categorical_crossentropy,
                   metrics=['accuracy'])
 
-    #########################################
+    return model
 
-    input2 = keras.layers.Input(shape=input_shape)
-    reshape2 = keras.layers.Reshape(target_shape=reshape_shape)(input2)
-    x2 = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same')(reshape2)
-    x2 = LAL.LearnedFourierActivationLayer(paramCount=4)(x2)
-    x2 = keras.layers.Dropout(rate=drop)(x2)
-    for _ in range(0, c):
-        x2 = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same')(x2)
-        x2 = LAL.LearnedFourierActivationLayer(paramCount=4)(x2)
-        x2 = keras.layers.Dropout(rate=drop)(x2)
-    x2 = keras.layers.Flatten()(x2)
+
+def learningModel(inputShape, c, d, h, outputLength, N, activationLayer):
+    input = keras.layers.Input(shape=inputShape)
+    x = input
+    for _ in range(0, c[0]):
+        for _ in range(0, c[1]):
+            x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(1, 1), padding='same', activation=None)(x)
+            x = activationLayer(N)(x)
+        x = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same', activation=None)(x)
+        x = activationLayer(N)(x)
+    x = keras.layers.Flatten()(x)
     for _ in range(0, d):
-        x2 = keras.layers.Dense(units=h, activation=None)(x2)
-        x2 = LAL.LearnedFourierActivationLayer(paramCount=4)(x2)
-    x2 = keras.layers.Dense(units=outputLength, activation=keras.activations.softmax)(x2)
+        x = keras.layers.Dense(units=h, activation=None)(x)
+        x = activationLayer(N)(x)
+    x = keras.layers.Dense(units=outputLength, activation=keras.activations.softmax)(x)
 
-    model2 = keras.Model(inputs=[input2], outputs=[x2])
-    model2.compile(optimizer=keras.optimizers.RMSprop(), loss=keras.losses.sparse_categorical_crossentropy,
+    model = keras.Model(inputs=[input], outputs=[x])
+    model.compile(optimizer=keras.optimizers.RMSprop(lr=0.0002), loss=keras.losses.sparse_categorical_crossentropy,
                   metrics=['accuracy'])
 
-    #########################################
-
-    input3 = keras.layers.Input(shape=reshape_shape)
-    reshape3 = keras.layers.Reshape(target_shape=reshape_shape)(input3)
-    x3 = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same', activation=act)(reshape3)
-    x3 = keras.layers.Dropout(rate=drop)(x3)
-    for _ in range(0, c):
-        x3 = keras.layers.Conv2D(filters=h, kernel_size=(3, 3), strides=(2, 2), padding='same', activation=act)(x3)
-        x3 = keras.layers.Dropout(rate=drop)(x3)
-    x3 = keras.layers.Flatten()(x3)
-    for _ in range(0, d):
-        x3 = keras.layers.Dense(units=h, activation=act)(x3)
-    x3 = keras.layers.Dense(units=outputLength, activation=keras.activations.softmax)(x3)
-
-    model3 = keras.Model(inputs=[input3], outputs=[x3])
-    model3.compile(optimizer=keras.optimizers.RMSprop(), loss=keras.losses.sparse_categorical_crossentropy,
-                  metrics=['accuracy'])
-
-    #print(model.predict(train_images))
-
-    model.fit(train_images, train_labels, epochs=5, verbose=True)
-    model2.fit(train_images, train_labels, epochs=5, verbose=True)
-    model3.fit(train_images, train_labels, epochs=5, verbose=True)
-
-    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=True)
-    test_loss2, test_acc2 = model2.evaluate(test_images, test_labels, verbose=True)
-    test_loss3, test_acc3 = model3.evaluate(test_images, test_labels, verbose=True)
-
-    print('(' + str(c) + ', ' + str(d) + ') Test accuracy:', test_acc)
-    print('(' + str(c) + ', ' + str(d) + ') Test accuracy 2:', test_acc2)
-    print('(' + str(c) + ', ' + str(d) + ') Test accuracy 3:', test_acc3)
-
-    return test_acc, test_acc2, test_acc3, '(' + str(c) + ', ' + str(d) + ') Test accuracy:' + str(test_acc), '(' + str(c) + ', ' + str(d) + ') Test accuracy 2:' + str(test_acc2), '(' + str(c) + ', ' + str(d) + ') Test accuracy 3:' + str(test_acc3)
+    return model
 
 
-testResults = []
-for i in range(0, 4):
-    arr = []
-    for j in range(0, 4):
-        arr.append(LALTester(c=i, d=j, act=keras.activations.tanh))
-    testResults.append(arr)
+def modelTester(c, d, h, N, learnedLayer, act=keras.activations.relu):
+    inShape = (32, 32, 3)
+    dataset = keras.datasets.cifar10
+    (train_images, train_labels), (test_images, test_labels) = dataset.load_data()
 
-print("Final Results:")
+    cm = controlModel(inShape, c, d, h, 10, act)
+    tm = tuningModel(inShape, c, d, h, 10, act)
+    lm = learningModel(inShape, c, d, h, 10, N, learnedLayer)
 
-for i in range(len(testResults)):
-    for j in range(len(testResults[i])):
-        print(testResults[i][j][3])
-        print(testResults[i][j][4])
-        print(testResults[i][j][5])
-        print()
+    print("Learning Model")
+    lm.fit(train_images, train_labels, epochs=5)
+    print("Tuning Model")
+    tm.fit(train_images, train_labels, epochs=5)
+    print("Control Model")
+    cm.fit(train_images, train_labels, epochs=5)
+
+    results = []
+    results.append(lm.evaluate(test_images, test_labels))
+    results.append(tm.evaluate(test_images, test_labels))
+    results.append(cm.evaluate(test_images, test_labels))
+
+    print(results)
+
+    return results
+
+
+test_results = []
+for _c in range(1, 4):
+    res_c = []
+    for _c2 in range(1, 4):
+        res_c2 = []
+        for _d in range(1, 4):
+            res_d = []
+            for _h in range(5, 8):
+                res_d.append(modelTester((_c, _c2), _d, 2**_h, 6, LAL.LearnedExponentialActivationLayer, keras.activations.relu))
+            res_c2.append(res_d)
+        res_c.append(res_c2)
+    test_results.append(res_c)
+
+
+for _c in range(0, 3):
+    for _c2 in range(0, 3):
+        for _d in range(0, 3):
+            for _h in range(0, 3):
+                print("Learned Activation (" + str(_c) + ", " + str(_c2) + ", " + str(_d) + ", " + str(2**(_h + 5)) + "): Loss = " + test_results[_c][_c2][_d][_h][0][0] + " Accuracy = " + test_results[_c][_c2][_d][_h][0][1])
+                print("Tuning  Activation (" + str(_c) + ", " + str(_c2) + ", " + str(_d) + ", " + str(2**(_h + 5)) + "): Loss = " + test_results[_c][_c2][_d][_h][1][0] + " Accuracy = " + test_results[_c][_c2][_d][_h][1][1])
+                print("Control Activation (" + str(_c) + ", " + str(_c2) + ", " + str(_d) + ", " + str(2**(_h + 5)) + "): Loss = " + test_results[_c][_c2][_d][_h][2][0] + " Accuracy = " + test_results[_c][_c2][_d][_h][2][1])
